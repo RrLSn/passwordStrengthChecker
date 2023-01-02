21a994b0ac49072c9c6c7084f501b28f84eeb55c
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-01-02 22:21:47 +0100

    checker

--- main.js
+++ main.js
@@ -18,18 +18,47 @@ const strength = (password) => {
     if(/[A-Za-z0-9]/.test(password)){
         i++
     }
+    if(/[!@#$%^&*()'+_=.,;/<>?":]/.test(password)){
+        i++
+    }
     return i
 }
 
 // selector 
 const container = document.querySelector('.container')
 const inputEl = document.querySelector('input')
-
-inputEl.setAttribute('id', 'myPassword')
+const showEl = document.querySelector('.show')
 
 document.addEventListener(
     'keyup',
     (e) => {
-        let password = strength(password)
+        let password = document.querySelector('#myPassword').value
+        let passStrength = strength(password)
+        if(passStrength <= 2){
+            container.classList.add('weak')
+            container.classList.remove('medium')
+            container.classList.remove('strong')
+        }else if (passStrength > 2 && passStrength <= 4){
+            container.classList.remove('weak')
+            container.classList.add('medium')
+            container.classList.remove('strong')
+        } else{
+            container.classList.remove('weak')
+            container.classList.remove('medium')
+            container.classList.add('strong')
+        }
     }
 )
+
+
+showEl.onclick = function () {
+    if(inputEl.type === 'password'){
+        inputEl.setAttribute('type', 'text')
+        showEl.classList.add('hide')
+        showEl.textContent = 'HIDE'
+    } else{
+        inputEl.setAttribute('type','password')
+        showEl.classList.remove('hide')
+        showEl.textContent = 'SHOW'
+    }
+}
